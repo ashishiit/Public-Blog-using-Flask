a3fae2c8a3bfb4a0efec8b2c1650ccb003fffdd6
ashishiit <s528358@nwmissori.com>
2020-03-08 18:47:43 +0530

    reinitialized files

---
+++ flaskblog/__init__.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy 
+from flask_bcrypt import Bcrypt 
+from flask_login import LoginManager
+from flask_mail import Mail
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '3f8603e349632a9b9da669b6b02b417e'
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = os.environ.get("EMAIL_USER")
+app.config["MAIL_PASSWORD"] = os.environ.get("EMAIL_PASS")
+mail = Mail(app)
+
+
+
+from flaskblog import routes 
